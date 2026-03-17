9914e0082df1997e6f83de0f36d35f541a0ee998
Chukwuka Agunwa <66865033+ZeJoker69@users.noreply.github.com>
2026-03-17 20:52:30 +0100

    initial upload
    i am deploying a website for my Uncle's business

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// PORTFOLIO FILTER
+
+const filterButtons = document.querySelectorAll(".filter-btn");
+const galleryItems = document.querySelectorAll(".gallery-item");
+
+filterButtons.forEach(btn => {
+
+btn.addEventListener("click", () => {
+
+document.querySelector(".active").classList.remove("active");
+btn.classList.add("active");
+
+const filter = btn.getAttribute("data-filter");
+
+galleryItems.forEach(item => {
+
+if(filter === "all" || item.classList.contains(filter)){
+item.style.display = "block";
+}else{
+item.style.display = "none";
+}
+
+});
+
+});
+
+});
+
+
+// LIGHTBOX
+
+const images = document.querySelectorAll(".gallery-item img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.querySelector(".lightbox-img");
+const closeBtn = document.querySelector(".close");
+
+images.forEach(img => {
+
+img.addEventListener("click", () => {
+
+lightbox.style.display = "flex";
+lightboxImg.src = img.src;
+
+});
+
+});
+
+closeBtn.addEventListener("click", () => {
+
+lightbox.style.display = "none";
+
+});
